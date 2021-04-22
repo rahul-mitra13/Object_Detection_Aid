726b068ef237628a7864994b09eea914af8aeeb9
Alisa Levin <alisalevin.nyc@gmail.com>
2021-04-21 21:37:42 -0400

    Added audio icons

--- ObjectDetectionAid/ObjectDetectionAid/ODAViewController.swift
+++ ObjectDetectionAid/ObjectDetectionAid/ODAViewController.swift
@@ -1,33 +1,3 @@
-/**
- * Copyright (c) 2017 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 import UIKit
 import CoreBluetooth
 import AVFoundation
@@ -36,7 +6,6 @@ import AVFoundation
 let nanoServiceCBUUID = CBUUID(string: "0xffff") //"180D"
 let objectDetectionCharacteristicCBUUID = CBUUID(string: "0xbbbb") //2A37
  // let bodySensorLocationCharacteristicCBUUID = CBUUID(string: "2A38") //don't need this
-
 
 class ODAViewController: UIViewController {
   
@@ -49,13 +18,15 @@ class ODAViewController: UIViewController {
   var centralManager: CBCentralManager!
   var nanoPeripheral: CBPeripheral!
   var count = 0
-  var dataReceivedFlag = false
+  var dataReceived = false
   var audioFlag = false
   
-
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
+    detectedObjectLabel.text = "Waiting..."
+    audio(label: "Waiting for connection")
+    
     centralManager = CBCentralManager(delegate: self, queue: nil)
 
     // Make the digits monospaces to avoid shifting when the numbers change
@@ -76,20 +47,32 @@ class ODAViewController: UIViewController {
   @objc func handleSwipes(_ sender:UISwipeGestureRecognizer) {
           
       if (sender.direction == .left) {
-          print("Swipe Left")
-          audioFlag = false
+        audioFlag = false
+        if dataReceived {
+          self.addImage(imageName: "blank")
+          self.addImage(imageName: "audio_off")
+        }
       }
           
       if (sender.direction == .right) {
-          print("Swipe Right")
           audioFlag = true
+          if dataReceived {
+            self.addImage(imageName: "blank")
+            self.addImage(imageName: "audio_on")
+          }
       }
   }
   
   func onDetectionReceived(_ detectedObject: Int) -> String {
     let dict = [0 : "Unlabeled", 1 : "Person", 2 : "Bicycle", 3 : "Car", 4 : "Motorcycle", 5 : "Airplane", 6 : "Bus", 7 : "Train", 8 : "Truck", 9 : "Boat", 10 : "Traffic Light", 11 : "Fire Hydrant", 12 : "Street Sign", 13 : "Stop Sign", 14 : "Parking Meter", 15 : "Bench", 16 : "Bird", 17 : "Cat", 18 : "Dog", 19 : "Horse", 20 : "Sheep", 21 : "Cow", 22 : "Elephant", 23 : "Bear", 24 : "Zebra", 25 : "Giraffe", 26 : "Hat", 27 : "Backpack", 28 : "Umbrella", 29 : "Shoe", 30 : "Eye Glasses", 31 : "Handbag", 32 : "Tie", 33 : "Suitcase", 34 : "Frisbee", 35 : "Skis", 36 : "Snowboard", 37 : "Sports Ball", 38 : "Kite", 39 : "Baseball Bat", 40 : "Baseball Glove", 41 : "Skateboard", 42 : "Surfboard", 43 : "Tennis Racket", 44 : "Bottle", 45 : "Plate", 46 : "Wine Glass", 47 : "Cup", 48 : "Fork", 49 : "Knife", 50 : "Spoon", 51 : "Bowl", 52 : "Banana", 53 : "Apple", 54 : "Sandwich", 55 : "Orange", 56 : "Broccoli", 57 : "Carrot", 58 : "Hot dog", 59 : "Pizza", 60 : "Donut", 61 : "Cake", 62 : "Chair", 63 : "Couch", 64 : "Potted Plant", 65 : "Bed", 66 : "Mirror", 67 : "Dining Table", 68 : "Window", 69 : "Desk", 70 : "Toilet", 71 : "Door", 72 : "TV", 73 : "Laptop", 74 : "Mouse", 75 : "Remote", 76 : "Keyboard", 77 : "Cell Phone", 78 : "Microwave", 79 : "Oven", 80 : "Toaster", 81 : "Sink", 82 : "Refrigerator", 83 : "Blender", 84 : "Book", 85 : "Clock", 86 : "Vase", 87 : "Scissors", 88 : "Teddy Bear", 89 : "Hair Drier", 90 : "Toothbrush", 95: " ", 96: " " ]
     
-    detectedObjectLabel.text = dict[detectedObject]
+    if audioFlag {
+      detectedObjectLabel.text = dict[detectedObject]
+    }
+    else {
+      detectedObjectLabel.text = "Audio Muted"
+    }
+    
     print("Object ID: \(String(describing: dict[detectedObject]!))")
     
     return dict[detectedObject]!
@@ -115,7 +98,7 @@ extension ODAViewController: CBCentralManagerDelegate {
       centralManager.scanForPeripherals(withServices: [nanoServiceCBUUID])
     }
   }
-
+  
   func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
                       advertisementData: [String : Any], rssi RSSI: NSNumber) {
     print(peripheral)
@@ -174,9 +157,10 @@ extension ODAViewController: CBPeripheralDelegate {
         
       let objectIDList = detectedObjects(from: characteristic)
       
-      if dataReceivedFlag == false {
-        audio(label: "Swipe right anywhere on the screen to hear audio feedback. Swipe left anywhere to turn audio off.")
-        dataReceivedFlag = true
+      if dataReceived == false {
+        detectedObjectLabel.text = "Connection Established"
+        audio(label: "Connection established. Swipe right anywhere on the screen to hear audio feedback. Swipe left anywhere to turn audio off.")
+        dataReceived = true
       }
       
       print("")
@@ -201,7 +185,7 @@ extension ODAViewController: CBPeripheralDelegate {
         
         //Add and check boolean flag here for whether audio is set to true or false
           if audioFlag {
-            if count % 12 == 0 { //reduces audio readout
+            if count % 15 == 0 { //reduces audio readout
               audio(label: fixGrammar(labels: objectLabels))
             }
           }
@@ -246,6 +230,23 @@ extension ODAViewController: CBPeripheralDelegate {
     let detectedIDs = [UInt8](characteristicData)
     return detectedIDs
   }
+  
+  func addImage(imageName img: String) {
+    let imageView = UIImageView(frame: CGRect(x: 130, y: 400, width: 150, height: 150))
+     if let newImage = UIImage(named: img) {
+      imageView.image = newImage
+     }
+     self.view.addSubview(imageView)
+  }
+  
+//  func removeImage() {
+//    let imageView = UIImageView()
+//    imageView.frame = self.view.frame
+//    imageView.contentMode = .scaleAspectFit
+//    imageView.image = nil
+//    self.view.addSubview(imageView)
+//  }
+
 }
 
 
